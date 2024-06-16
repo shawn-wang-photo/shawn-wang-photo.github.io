4e676ba54a00e683f6a89f5462e2d07c15e425ee
Zixiao Wang <zixiaowang@Zixiaos-MacBook-Pro.local>
2024-06-15 21:01:51 -0700

    implement nav bar and import font

---
+++ my-app/src/Home.tsx
@@ -0,0 +1,10 @@
+import './Home.css'
+
+function Home() {
+    return (
+        <div className="homePageContainer">
+        </div>
+    ); 
+}
+
+export default Home;
---
+++ my-app/src/NavigationBar.tsx
@@ -0,0 +1,22 @@
+import './NavigationBar.css';
+import { ReactComponent as InstagramIcon } from './icon/instagram.svg';
+
+function NavigationBar() {
+    return (
+        <div className="navigationBar">
+            <div className="title">Shawn Wang</div>
+            <div className="categories">
+                <div className='category'>STREET</div>
+                <div className='category'>PORTRAITS</div>
+                <div className='category'>OUTDOORS</div>
+                <div className='category'>FILM MAKING</div>
+            </div>
+            <div className='footer'>
+                <div className="about">About</div>
+                <InstagramIcon />
+            </div>
+        </div>
+    );
+}
+
+export default NavigationBar;
