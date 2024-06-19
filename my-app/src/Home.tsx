aac7793ac3c553366cdf00b85225bcba6adc1a26
Zixiao Wang <zixiaowang@Zixiaos-MacBook-Pro.local>
2024-06-19 00:13:17 -0700

    add home page images

--- my-app/src/Home.tsx
+++ my-app/src/Home.tsx
@@ -3,6 +3,10 @@ import './Home.css'
 function Home() {
     return (
         <div className="homePageContainer">
+            <div className="imageContainer">
+                <img className="homeImage" src={"./images/santa_cruz.jpg"} alt="cover_image"/>
+                <div className="caption">Santa Cruz, 2022</div>
+            </div>
         </div>
     ); 
 }
