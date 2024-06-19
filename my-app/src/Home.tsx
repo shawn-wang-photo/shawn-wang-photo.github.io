b790970d96131966feb60abdf965d31ff06cee4d
Zixiao Wang <zixiaowang@Zixiaos-MacBook-Pro.local>
2024-06-19 13:03:15 -0700

    home images rotate

--- my-app/src/Home.tsx
+++ my-app/src/Home.tsx
@@ -1,11 +1,23 @@
+import { useEffect, useState } from 'react';
 import './Home.css'
+import { HOME_IMAGES } from './constants';
 
 function Home() {
+    const [index, setIndex] = useState<number>(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIndex((prevIndex) => (prevIndex + 1) % HOME_IMAGES.length);
+        }, 5000)
+
+        return () => {clearInterval(interval);}
+    }, []);
+
     return (
         <div className="homePageContainer">
-            <div className="imageContainer">
-                <img className="homeImage" src={"./images/santa_cruz.jpg"} alt="cover_image"/>
-                <div className="caption">Santa Cruz, 2022</div>
+            <div className={`imageContainer fadeIn`} key={index}>
+                <img className="homeImage" src={HOME_IMAGES[index].src} alt="cover_image"/>
+                <div className="caption">{HOME_IMAGES[index].caption}</div>
             </div>
         </div>
     ); 
---
+++ my-app/src/constants.ts
@@ -0,0 +1,35 @@
+type ImageWithCaption = {
+    src: string;
+    caption: string;
+}
+
+export const HOME_IMAGES: ImageWithCaption[] = [
+    {
+        src: './images/birds.jpg',
+        caption: 'Santa Cruz, 2022'
+    },
+    {
+        src: './images/women_march.jpg',
+        caption: "Women's March, Civic Center, 2022",
+    },
+    {
+        src: './images/vegas_double_exposure.jpg',
+        caption: "Double Exposure, Las Vegas, 2024",
+    },
+    {
+        src: './images/foster_city.jpg',
+        caption: 'A Couple, Fostery City, 2022'
+    },
+    {
+        src: './images/golden_gate_bird.jpg',
+        caption: 'Golden Gate Bridge, 2022'
+    },
+    {
+        src: './images/pride.jpg',
+        caption: 'Pride Parade, San Francisco, 2021'
+    },
+    {
+        src: './images/santa_cruz_boat.jpg',
+        caption: 'Santa Cruz, 2022'
+    },
+];
