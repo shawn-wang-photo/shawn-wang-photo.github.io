56bd8547a983c764e75e483964e2e561f4866d7c
Zixiao Wang <zixiaowang@Zixiaos-MacBook-Pro.local>
2024-08-03 13:37:31 -0700

    about ignore

--- my-app/src/About.tsx
+++ my-app/src/About.tsx
@@ -3,7 +3,18 @@ import './About.css';
 function About() {
     return (
         <div className='aboutPageContainer'>
-            About Page
+            <div className='aboutBlock'>
+                <img className='aboutImage' src={'./images/self_portrait.jpg'} alt='self_portrait' />
+                <div className='aboutCaption'>Belmont, 2024</div>
+                <div className='aboutText'>
+                    Web engineer by day, Shawn Wang is a self-taught photographer and videographer in his free time.
+                    Originally from mainland China, he now lives in Bay Area, California.
+                    His work spans a wide range of genres with different visual mediums.
+                    Whether on city streets, perilous mountain expeditions or against studio backdrop,
+                    his lens focuses on the visceral moments and raw essence of each setting.
+                </div>
+
+            </div>
         </div>
     );
 }
--- my-app/src/Filmmaking.tsx
+++ my-app/src/Filmmaking.tsx
@@ -4,8 +4,26 @@ import YoutubeEmbed from './YoutubeEmbed';
 function Filmmaking() {
     return (
         <div className='filmmakingPageContainer'>
-            <YoutubeEmbed embedId='idhymYvRhW0' />
-            <YoutubeEmbed embedId='pmws__OR_Vs' />
+            <div className='videoBlock' key='idhymYvRhW0'>
+                <YoutubeEmbed embedId='idhymYvRhW0' />
+                <div className='videoTitle'>
+                    Hold On Maria, We're Gonna Dance(Kinda Cover) - Bear Bleach
+                </div>
+                <div className="videoDescription">
+                    August 2023. <a target='_blank' href='https://www.youtube.com/@yifan1121' rel="noreferrer">Yifan</a>, Qifang and I took on a mission filming a live session for a local band at Guitar War studio in San Jose.
+                    The band is called <a href='https://www.youtube.com/channel/UC4Kzi8kaJmk77Z_3yzooZmQ' target='_blank' rel="noreferrer">Bear Bleach</a> and it has four talented and passionate members, with Michael being the vocal and playing guitar, Jonathan on bass, Brian on drum and Zihao on synth and keyboard.
+                </div>
+            </div>
+            <div className='videoBlock' key='pmws__OR_Vs'>
+                <YoutubeEmbed embedId='pmws__OR_Vs' />
+                <div className='videoTitle'>
+                    Dead Sound - The Raveonettes Tour
+                </div>
+                <div className="videoDescription">
+                    June 2024. I made a small documentary style music video for The Raveonettes during their tour in San Francisco. Footage and audio are recorded with a simple Panasonic camcorder.
+                    The style of the editing is heavily influenced by the tour diaries from <a href='https://www.youtube.com/@theraveonettes/featured' rel='noreferrer' target="_blank">The Raveonettes Youtube channel</a> and other tour documentaries. 
+                </div>
+            </div>
         </div>
     );
 }
